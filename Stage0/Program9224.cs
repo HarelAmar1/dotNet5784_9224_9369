be73a729192694b30093330e9c7cd101f99fdfa6
Harel amar <arelamar1@gmail.com>
2023-12-24 23:06:32 +0200

    Initial code + Welcome functions

---
+++ Stage0/Program9224.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Stage0
+{
+    partial class Program
+    {
+        private static void Main(string[] args)
+        {
+            Welcome9224();
+            Welcome9369();
+            Console.ReadKey();
+        }
+        static partial void Welcome9369();
+        private static void Welcome9224()
+        {
+            Console.WriteLine("Enter your name please:");
+            string name = Console.ReadLine();
+            Console.WriteLine("welcome {0}!", name);
+        }
+    }
+}
