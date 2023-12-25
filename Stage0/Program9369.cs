575df8cb8f318cb21c7175af2decb48602e5e6bc
shneor mizrachi <shneor146@gmail.com>
2023-12-25 10:37:51 +0200

    Initial Code + Welcome functions

---
+++ Stage0/Program9369.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace Stage0
+{
+    partial class Program
+    {
+        static partial void Welcome9369()
+        {
+            Console.WriteLine("I am also here!");
+        }
+    }
+}
