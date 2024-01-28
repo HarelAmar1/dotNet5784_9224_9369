19a8dc1954eb8e7a05241329c3f64de185095a32
Harel amar <arelamar1@gmail.com>
2024-01-28 16:22:57 +0200

    I almost finished writing the IMilestone interface in the BLApi library

---
+++ BL/BlApi/IMilestone.cs
@@ -0,0 +1,13 @@
+﻿
+using BO;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Intrinsics.X86;
+namespace BlApi;
+
+public interface  IMilestone
+{
+    public IEnumerable<BO.MilestoneInList> SeeTheListOfMilestones();
+    public void UpdateMilestoneDetails();
+    public IEnumerable<BO.MilestoneInList> ReturnsTheEngineerListOfMilestones();
+}
