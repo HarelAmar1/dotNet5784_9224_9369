f5b3063ef8469f3b3246fbf964277d9f09574146
Harel amar <arelamar1@gmail.com>
2024-01-28 15:43:14 +0200

    I finished writing the IEngineer interface in the BLApi library

---
+++ BL/BlApi/IEngineer.cs
@@ -0,0 +1,14 @@
+﻿namespace BlApi;
+
+public interface IEngineer
+{
+    public IEnumerable<BO.EngineerInTask> GettingTheListOfEngineer();
+
+    public IEnumerable<BO.Engineer> EngineerDetailsRequest(int id);
+
+    public void AddingAnEngineer(IEnumerable<BO.Engineer> NeedtoAdd);
+
+    public void DeletingAnEngineer(int id);
+
+    public void UpdatingAnEngineer(IEnumerable<BO.Engineer> NeedToUpdate);
+}
