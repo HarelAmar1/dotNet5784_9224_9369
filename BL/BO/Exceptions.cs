77999fa55c013f7808b4f7b7a566067ce412f1d3
shneor mizrachi <shneor146@gmail.com>
2024-01-31 15:22:11 +0200

    fix Exception

--- BL/BO/Exceptions.cs
+++ BL/BO/Exceptions.cs
@@ -1,31 +1,38 @@
-﻿namespace BO
+﻿namespace BO;
+
+
+[Serializable]
+public class BlDoesNotExistException : Exception
 {
- 
-    [Serializable]
-    public class BLDoesNotExistException : Exception
-    {
-        public BLDoesNotExistException(string? message) : base(message) { }
-    }
-    [Serializable]
-    public class BLAlreadyExistsException : Exception
-    {
-        public BLAlreadyExistsException(string? message) : base(message) { }
-    }
-    [Serializable]
-    public class BLDeletionImpossible : Exception
-    {
-        public BLDeletionImpossible(string? message) : base(message) { }
-    }
-    [Serializable]
-    public class BLCanNotBeNULL : Exception
-    {
-        public BLCanNotBeNULL(string? message) : base(message) { }
-    }
-
-    public class BLXMLFileLoadCreateException : Exception
-    {
-        public BLXMLFileLoadCreateException(string? message) : base(message) { }
-    }
-
+    public BlDoesNotExistException(string? message) : base(message) { }
+    public BlDoesNotExistException(string message, Exception innerException): base(message, innerException) { }
 }
+[Serializable]
+public class BlAlreadyExistsException : Exception
+{
+    public BlAlreadyExistsException(string? message) : base(message) { }
+    public BlAlreadyExistsException(string message, Exception innerException) : base(message, innerException) { }
+}
+[Serializable]
+public class BlDeletionImpossible : Exception
+{
+    public BlDeletionImpossible(string? message) : base(message) { }
+    public BlDeletionImpossible(string message, Exception innerException) : base(message, innerException) { }
+}
+[Serializable]
+public class BlCanNotBeNULL : Exception
+{
+    public BlCanNotBeNULL(string? message) : base(message) { }
+    public BlCanNotBeNULL(string message, Exception innerException) : base(message, innerException) { }
+}
+
+public class BlXMLFileLoadCreateException : Exception
+{
+    public BlXMLFileLoadCreateException(string? message) : base(message) { }
+    public BlXMLFileLoadCreateException(string message, Exception innerException) : base(message, innerException) { }
+}
+public class BlTheInputIsIncorrect : Exception
+{
+    public BlTheInputIsIncorrect(string? message) : base(message) { }
+    public BlTheInputIsIncorrect(string message, Exception innerException) : base(message, innerException) { }
 }
