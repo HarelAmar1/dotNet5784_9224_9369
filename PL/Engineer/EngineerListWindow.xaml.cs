ac60eed658bbf0ebcc6750c6abfaff86dbfa4a6b
Harel amar <arelamar1@gmail.com>
2024-02-11 00:55:50 +0200

    done EP5

---
+++ PL/Engineer/EngineerListWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PL.Engineer
+{
+    /// <summary>
+    /// Interaction logic for EngineerListWindow.xaml
+    /// </summary>
+    public partial class EngineerListWindow : Window
+    {
+        static readonly BlApi.IBl s_bl = BlApi.Factory.Get();
+        public EngineerListWindow()
+        {
+            InitializeComponent();
+            EngineerList = s_bl?.Engineer.ReadAll()!;
+        }
+
+        public IEnumerable<BO.Engineer> EngineerList
+        {
+            get { return (IEnumerable<BO.Engineer>)GetValue(EngineerListListProperty); }
+            set { SetValue(EngineerListListProperty, value); }
+        }
+
+        public static readonly DependencyProperty EngineerListListProperty =
+            DependencyProperty.Register("EngineerList", typeof(IEnumerable<BO.Engineer>), typeof(EngineerListWindow), new PropertyMetadata(null));
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
--- PL/MainWindow.xaml.cs
+++ PL/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using PL.Engineer;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,6 +20,27 @@ namespace PL
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnEngineer_Click(object sender, RoutedEventArgs e)
+        {
+            new EngineerListWindow().Show();
+        }
+
+        private void InitDB(object sender, RoutedEventArgs e)
+        {
+            new EngineerListWindow().Show();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxButton buttons = MessageBoxButton.OKCancel;
+            MessageBoxResult a = MessageBox.Show("Would you like to create Initial data?", "cho", buttons);
+            if (a == MessageBoxResult.OK)
+            {
+                DalTest.Initialization.deleteXMLFile();
+                DalTest.Initialization.Do();
+            }
         }
     }
 }
